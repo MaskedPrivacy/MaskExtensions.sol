d18db33d480dd103e2e39fabc9e3c1e88b1b17b7
Masked Privacy Lead Developer <79077089+MaskedPrivacy@users.noreply.github.com>
2021-03-02 15:58:38 -0500

    Update BurnExtension.sol

--- BurnExtension.sol
+++ BurnExtension.sol
@@ -2,7 +2,7 @@ import "Token.sol";
 
 /*
 Masked Token (MASK)
-Feb. 2021
+March. 2021
 Created by Masked Privacy
 
 */
@@ -17,15 +17,18 @@ contract MaskedExtensions {
     
     Mask private Extension;
     
-    mapping (address => uint256) private _balances;
+    mapping (uint256 => bool) public _votes;
+    
+    uint256 public voteCount;
     
     constructor(address _MaskedAddress, address _Owner) public 
     {
         ContractAddress = _MaskedAddress;
         Owner = _Owner;
+        voteCount = 0;
     }
     
-    function Existing(address _t) public {
+    function Existing(address payable _t) public {
         Extension = Mask(_t);
     }
  
@@ -37,8 +40,27 @@ contract MaskedExtensions {
         Extension._burn(account,amount);
         
     }
+
+    function ballotTest(bool decision) public payable
+    {
+        require(Extension.balanceOf(msg.sender) > 0);
+        
+        Extension._burn(msg.sender, 1 * 10e18);
+        
+        if(decision == true)
+            _votes[voteCount] = true;
+        else
+            _votes[voteCount] = false;
+        
+        voteCount = voteCount + 1;
+    }
+    
+    function transferOutERC20Token(address _tokenAddress, uint256 _tokens) public returns (bool success) {
+        return IERC20(_tokenAddress).transfer(msg.sender, _tokens);
+    }     
     
 }
+
 
 
 
