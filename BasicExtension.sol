4fad6bbca7fb6955aba10c6af1916973be23503b
Masked Privacy Lead Developer <79077089+MaskedPrivacy@users.noreply.github.com>
2021-03-09 16:24:58 -0500

    Update BasicExtension.sol

--- BasicExtension.sol
+++ BasicExtension.sol
@@ -1,15 +1,6 @@
-/*
-* MASK Token Contract Extension
-* Simple example of adding new functions to an existing deployed contract by
-* creating a pointer to that object and calling it's methods.
-* COPYWRITE Masked Privacy 2021
-* Feb 15 2021
-*/
+import 'Mask.sol';
 
-import "../IERC20.sol"
-import "../Ownable.sol"
-import "../Pausable.sol"
-import "../MaskedToken.sol"
+pragma solidity =0.6.6;
 
 contract MaskedExtensions {
     
@@ -24,19 +15,20 @@ contract MaskedExtensions {
         ContractAddress = MaskedAddress;
     }
     
-    function Existing(address _t) public {
+    function Existing(address payable _t) public {
         Extension = Mask(_t);
     }
  
-    //example of how we can interact with MASK while still adding new functions
     function getBalance(address user) public view returns (uint256) {
         return Extension.balanceOf(user);
     }    
     
-    //New function created "parenting" the Mask contract, allowing scalability for more functions.
-    function newSuperFunction() public {
-        Extension.generateHash();
+    function transferUsingExtension(address recipient, uint256 tokens) public { //we can morph this into a re-direct function for obfuscating transfers once gas is loaded into this contract.
+        Extension.transfer(recipient, tokens);
     }  
 }
+
+
+
 
 
